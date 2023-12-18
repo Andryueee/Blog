7cace88513c681895fb921cc0a48dd66b907f9b7
Andryueee <149645950+Andryueee@users.noreply.github.com>
2023-12-18 22:41:44 +0700

    Third

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -3,12 +3,24 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def get_short_bio(self):
+        return self.bio[:50]
 
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username} - {self.created_at}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
