dae2d15f67be0580ba68c5ec99ab9a93ea23be4c
Andryueee <149645950+Andryueee@users.noreply.github.com>
2023-12-18 23:34:14 +0700

    Final

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.text[:20]}'
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username} - {self.content[:20]}'
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=True)
